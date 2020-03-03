c841cbfc4c3387e75314d1ecd5beeae71c1f60e7
SorinEni <60381859+SorinEni@users.noreply.github.com>
2020-03-03 09:30:50 +0100

    Update index.php
    doplneni obrazku

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php
 
-$age = 15;
+$age = 12;
+$ano = "https://im9.cz/iR/importprodukt-orig/63f/63ff97a826ab65a9b150b3c10558f015.jpg";
 
 
 if ($age >= 18 ) {
-   $answer = "Děkujeme za kšeft, přijdte zas.";
+   $answer = "Děkujeme za kšeft, přijdte zas."; $ano = "https://im9.cz/iR/importprodukt-orig/63f/63ff97a826ab65a9b150b3c10558f015.jpg";
 }
 
 else {
-  $answer = "Máš smůlu chlapče alkohol a tabák prodáváme od 18ti let.";
+  $answer = "Máš smůlu chlapče alkohol a tabák prodáváme od 18ti let."; $ano = "https://im9.cz/iR/importprodukt-orig/205/2051e75aa29cdb632dac36c21943da02.jpg" ;
 }
 
 ?>
@@ -23,8 +24,8 @@ else {
 <body>
     
 <p> Kolik ti je?</p>
-<p> Je mi <?= $age; ?> let</p>
-<p> <?= $answer; ?> </p>
+<p> Je mi <?= $age; ?> let</p> <img src="<?= $ano?>" alt="..." />
+<p> <?= $answer; ?> </p>  
 
 
 
